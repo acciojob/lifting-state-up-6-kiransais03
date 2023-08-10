67cb44b548f372c7e852a9d938458a9a0615d41c
kiransai <kiransais03@gmail.com>
2023-08-10 15:57:02 +0530

    submitting code-Thu Aug 10 2023 15:57:00 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,13 +2,18 @@ import React,{useState} from "react";
 
 const Child = ({state,setState})=>{
 let {learnreact,buildreact,deployreact} = state
-console.log(state)
+console.log(state);
+
+
 return (<>
     <h2>Child Component</h2>
    <ul>
-    <li>Learn React {learnreact && <button onClick={(e)=>{setState({...state,learnreact:""});e.target.style.display="none"}}>Complete</button>}</li>
-    <li>Build a React App {buildreact && <button onClick={(e)=>{setState({...state,buildreact:""});e.target.style.display="none"}}>Complete</button>}</li>
-    <li>Deploy the React App {deployreact && <button onClick={(e)=>{setState({...state,deployreact:""});e.target.style.display="none"}}>Complete</button>}</li>
+    <li>Learn React {learnreact && <button onClick={(e)=>{
+         setState({...state,learnreact:""})}}>
+            Complete</button>}</li>
+
+    <li>Build a React App {buildreact && <button onClick={(e)=>{setState({...state,buildreact:""})}}>Complete</button>}</li>
+    <li>Deploy the React App {deployreact && <button onClick={(e)=>{setState({...state,deployreact:""})}}>Complete</button>}</li>
    </ul>
     </>)
 }
